80ef2f6bf69f630a63b2846d68cc4f2f11a3d13c
jakejubjub1 <jakejubjub@gmail.com>
2016-04-18 18:01:10 -0400

    time_t to int and recurring boolean+getter/setter

--- event.h
+++ event.h
@@ -33,15 +33,15 @@ class Event{
     friend class calender;
 
     public:
-        Event(time_t start, time_t end,char* location);
+        //Event(time_t start, time_t end,char* location);
         Event();
         virtual ~Event();
         int menu();
         vector<Event> fillVector(vector<Event>& eventList);
         void getLocation(vector<Event>& eventList,int index);
         void getUserinfo();
-        time_t getStartTime(vector<Event>& eventList,int index);
-        void getEndTime(time_t start,vector<Event>& HardEvent,int index);
+        int getStartTime(vector<Event>& eventList,int index);
+        void getEndTime(int start,vector<Event>& HardEvent,int index);
         void getDate(vector<Event>& HardEvent, int index,int month, int day);
         void makeChange(vector<Event>& HardEvent);
         int pickChangeField(vector<Event>& HardEvent, int choice);
@@ -59,18 +59,18 @@ class Event{
         void set_location_y(double a);
         unsigned int get_duration();
         unsigned int set_duration(unsigned int a);
-        time_t get_start_time();
-        void set_start_time(time_t a);
+        int get_start_time();
+        void set_start_time(int a);
         int get_end_time();
         void set_end_time(int a);
         char* get_location_name(int i);
         void set_location_name();
         void set_user_loaction(char* i);
         char* get_user_loaction();
-        void set_sleep_time_begin(time_t slee_time_begin);
-        time_t get_sleep_time_begin();
-        void set_sleep_time_end(time_t slee_time_end);
-        time_t get_sleep_time_end();
+        void set_sleep_time_begin(int slee_time_begin);
+        int get_sleep_time_begin();
+        void set_sleep_time_end(int slee_time_end);
+        int get_sleep_time_end();
         int get_day();
         void set_day(int a);
         int get_month();
@@ -78,21 +78,24 @@ class Event{
         char* get_task();
         void set_task(char* a);
         void set_hard_task_list();
+        void set_recurring(bool r);
+        bool get_recurring();
 
     protected:
 
     private:
+        bool recurring;
         int month;
         int day;
         double location_x;
         double location_y;
         unsigned int duration;
-        time_t start_time;
-        time_t end_time;
+        int start_time;
+        int end_time;
         char** location_name;///name of the location that event happends
         char* subject_name;///class or go to gym
-        time_t sleep_time_begin;
-        time_t sleep_time_end;
+        int sleep_time_begin;
+        int sleep_time_end;
         char* user_loaction;
         char* task;
         char** hard_task_list;
