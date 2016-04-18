bfe9ee1e8d3357c3c9296b5afef5bc0d60fcde12
Bakushin10 <shnnagai@gmail.com>
2016-04-18 15:33:11 -0400

    Update calender.h

--- calender.h
+++ calender.h
@@ -10,17 +10,28 @@ using namespace std;
 
 class calender{
 
-
-
     public:
         calender(int size);
         virtual ~calender();
         void showCalender(vector<Event>& eventList,vector<Soft>& softEvent,int month,int day);
-        void getTimeFrame(vector<Event>& eventList,char** calenderChar,bool* calenderBool,int day,int index);
+        void getTimeFrameHardEvent(vector<Event>& eventList,char** calenderChar,bool* calenderBool,int day,int index);
+        void getTimeFrameSoftEvent(vector<Soft>& softEvent,int day);
+        void isFitInSchedule(vector<Soft>& softEvent,int day,int index,bool* schedule);
+        void fitSoftTask(vector<Soft>& softEvent,int startTimeFrame,int endTimeFrame,int index);
+        void findEarliestTimeFrame(int due,int duration,int& startTimeFrame,int& endTimeFrame,bool* schedule,int&consec,int& maxConsec);
+        void fintBestFit(int due,int duration,int& startTimeFrame,int& endTimeFrame,bool* schedule,int&consec,int& maxConsec);
 
+        int get_counter();
+        void increment_counter();
+
+         bool getCalenderBool(int a);
     protected:
     private:
         char** calenderChar;
+        int counter;///keep track of softwork
+
+        ///true = space taken
+        ///false = space avalible
         bool* calenderBool;
 };
 
