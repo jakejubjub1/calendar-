1e0a88a0bb557872f62bc676619d432e07513013
Bakushin10 <shnnagai@gmail.com>
2016-04-19 20:04:42 -0400

    Update newSleepingTimeChang.h

--- newSleepingTimeChang.h
+++ newSleepingTimeChang.h
@@ -3,10 +3,9 @@
 
 #include "calender.h"
 
-class newSleepTimeChang : public calender {
-
+class newSleepTimeChang{
     public:
-        newSleepTimeChang();
+        newSleepTimeChang(calender* calen);
         virtual ~newSleepTimeChang();
         int get_new_sleep_start();
         void set_new_sleep_start(int a);
@@ -14,11 +13,12 @@ class newSleepTimeChang : public calender {
         void set_new_sleep_end(int a);
         void get_new_sleep(int month,int day);
         int getChnageDay(int month,int day);
-        int getSleepTime(int num,int i);
+        int getSleepTime(int num,int i,int day);
         bool validTimeFrame(int time);
         bool validTimeFrameEnd(int time,int new_ss);
 
     protected:
+        calender* calen;
         int new_sleep_start;
         int new_sleep_end;
         int* sleepStarts;
